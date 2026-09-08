b3fe28d892a24a9434718e8d6ac1475dcfc6f147
Alexandre Farias <alexandrearfarias@gmail.com>
2026-09-08 10:32:56 -0300

    add: ciração de esturtura pro app e adição do angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'nfse',
+        pathMatch: 'full'
+    },
+    {
+        path: 'nfse',
+        loadComponent: async () => {
+            return import('./features/nfse/pages/consulta/consulta').then(m => m.Consulta)
+        }
+    }
+];
---
+++ src/app/features/nfse/pages/consulta/consulta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-consulta',
+  styleUrl: './consulta.scss',
+  templateUrl: './consulta.html',
+})
+export class Consulta {}
